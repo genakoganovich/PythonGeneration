85f7895294cd2801a746460a860e5615cc01f9f5
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-06-20 22:04:08 +0300

    0009: 9.6.3.caesar_cipher.py done

--- 9.Strings/9.6.memory_unicode/9.6.3.caesar_cipher.py
+++ 9.Strings/9.6.memory_unicode/9.6.3.caesar_cipher.py
@@ -1,17 +1,4 @@
-# n = int(input())
-# s = input()
-#
-# list(map(lambda x: print(chr(ord(x) - n), end=''), s))
+n = int(input())
+s = input()
 
-s = 'fsfftsfufksttskskt'
-t = 'rerrfergrweffewewf'
-
-
-list(map(lambda i: print(str(ord(t[i])), end=' '), range(len(s))))
-print()
-
-list(map(lambda i: print(str(ord(s[i])), end=' '), range(len(s))))
-print()
-
-
-list(map(lambda i: print(str(ord(t[i]) - ord(s[i])), end=' '), range(len(s))))
+list(map(lambda x: print(chr(ord('a') + (ord(x) - ord('a') + 26 - n) % 26) , end=''), s))
